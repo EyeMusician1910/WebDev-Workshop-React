c3e27e5a36e78c567281a0f20c7502accac97e30
EyeMusician1910 <dp19102004d@gmail.com>
2025-03-07 11:07:23 +0530

    Final Project

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,16 @@
-const express = require('express');
+import express from 'express';
+const app = express();
+const port = 3000;
+
+import productRoutes from './src/routes/productRoutes.js'
+app.use(express.json());
+app.use('/products', productRoutes);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ Backend/src/routes/productRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { addProduct, getallProducts } from '../controllers/productController.js'
+
+
+const router = express.Router();
+
+router.get('/',getallProducts);
+router.post('/',addProduct);
+export default router;
